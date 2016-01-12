8ca4bb82527e0e163b29bbc9816a255b61dc4041
Toofpic <nick008ru@gmail.com>
2016-01-12 05:48:33 +0300

    added template

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=273we4d3hb)glakcgl#l3$h2(n^s1u7pd9x&#5)utg+65ql3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- photosite/views.py
+++ photosite/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'photosite/post_list.html', {})
